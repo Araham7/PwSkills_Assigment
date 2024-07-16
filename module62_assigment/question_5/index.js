aeec8c6ea3779375261cfec7dca701ae86dca4ea
Araham Abeddin <108493882+Araham7@users.noreply.github.com>
2024-07-16 16:08:32 +0530

    Add files via upload

--- module62_assigment/question_5/index.js
+++ module62_assigment/question_5/index.js
@@ -1,1 +1,14 @@
-...
+const fs = require('fs');
+
+// deletion of file :---
+fs.unlink("nodejs_architecture.txt", (err) => {
+    if (err) {
+        console.error('Error deleting file:', err);
+        return;
+    }
+    console.log("File Deleted SuccessFully");
+});
+
+
+
+// Sir please run index.js file and delete the "nodejs_architecture.txt" file .
