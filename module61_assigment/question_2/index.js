9808a24c59225217b2490e37b1fef9e56d9b3db2
Araham Abeddin <108493882+Araham7@users.noreply.github.com>
2024-07-16 14:43:43 +0530

    Add files via upload

--- module61_assigment/question_2/index.js
+++ module61_assigment/question_2/index.js
@@ -1,1 +1,17 @@
-.
+const fs = require('fs');
+const os = require('os');
+
+const arc = os.arch();
+
+console.log(arc); // OUTPUT: x64
+
+const textToWrite = `Your OS Architecture is > ${arc}`;
+
+// Writing in nodejs_architecture.txt
+fs.writeFile('nodejs_architecture.txt', textToWrite, (err) => {
+    if (err) {
+        console.log(`Error writing file: ${err}`);
+    } else {
+        console.log('File created and written your OS architecture successfully!');
+    }
+});
