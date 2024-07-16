76abbc1bd063745862fc58f06f4602c1bc256d7e
Araham Abeddin <108493882+Araham7@users.noreply.github.com>
2024-07-16 17:56:11 +0530

    Update index.js

--- module62_assigment/question_3/index.js
+++ module62_assigment/question_3/index.js
@@ -1,18 +1,7 @@
 const fs = require('fs');
 
-const textToAppend = `Compared to other server-side languages, Node js has distinct advantages. Its asynchronous model and non-blocking I/O operation improve the scalability and performance of web applications built on other frameworks. Node js can easily handle multiple client requests without requiring multiple threads, consuming less memory and resources. Additionally, it is highly scalable and provides high performance. Node js is also flexible with multiple frameworks and makes the development process easier.` 
-
-// Asynchronously append text to a file :---
-fs.appendFile('nodejs_architecture.txt', textToAppend , (err) => {
-    if (err) {
-        console.error('Error appending to the file:', err);
-        return;
-    }
-    console.log('Text appended successfully!');
-});
-
 // Reading the modified file :---
-    fs.readFile('nodejs_architecture.txt' , 'utf-8', (err , data)=>{
+    fs.readFile('./nodejs_architecture.txt' , 'utf-8', (err , data)=>{
         if (err) {
             console.log(`Error reading the file > ${err}`);
             return
